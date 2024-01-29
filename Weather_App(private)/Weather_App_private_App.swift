13e069cece330cff7caaf5f8270d276e3708d7a6
arya_irse@yahoo.com <arya_irse@yahoo.com>
2024-01-29 17:43:14 +0100

    Initial Commit

---
+++ Weather_App(private)/Weather_App_private_App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_App_private_App.swift
+//  Weather_App(private)
+//
+//  Created by Arya Pour Eisa on 2024-01-29.
+//
+
+import SwiftUI
+
+@main
+struct Weather_App_private_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
